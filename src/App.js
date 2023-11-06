ee6ec1ceb1355bbc2ed678f450d6ec5cc4817b02
DarshanGhorpade2003 <darshankumarghorpade28@gmail.com>
2023-11-06 14:01:01 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Library from './images/Library.jpg';
 import './App.css'
 function App() {
   return (
-    <div  >
+    <div>
        <div className='header'>
         <h1 className='contact'> 08373-26643,262075 </h1>
         <h1 className='contact'> Fax:08373-266427 </h1>
@@ -25,7 +25,7 @@ function App() {
     <div>
     
       <div className='nav'>
-        <img src= {Stjit} alt=''></img>
+        <img src= {Stjit} alt='i'></img>
         <h2 classname='footer'>Home</h2>
         <h3 className='footer'>About</h3>
         <h3 className='footer'>Admissions</h3>
@@ -33,19 +33,19 @@ function App() {
         <h3 className='footer'>Placements</h3>
       </div>
       <div >
-        <img src= {Campus} alt=''></img><br></br>
+        <img src= {Campus} alt='c'></img><br></br>
         <h3 classname="Photo">Welcome To Our College</h3>
       </div>
       <div className="First">
-      <img src= {Front} alt=''></img>
-      <img src= {Circle} alt=''></img>
+      <img src= {Front} alt='a'></img>
+      <img src= {Circle} alt='b'></img>
       </div>
       <div className='Second'>
-      <img src= {Swamiji} alt=''></img><br></br>
+      <img src= {Swamiji} alt='d'></img><br></br>
       </div> 
       <div className="Third">
       <img src={Lab} alt=''></img>
-      <img src={Library} alt=' '><img>
+      <img src={Library} alt='e'><img>
       </div>
       <div classname="Profile">
        <h1 className="Profile"> A Commitment to Technical Education </h1> 
